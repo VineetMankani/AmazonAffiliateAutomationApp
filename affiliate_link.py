a7417a243d2ab4cfe21199ccff5b206f4d6a1ed0
Vineet Mankani <76684670+VineetMankani@users.noreply.github.com>
2025-03-02 01:42:15 +0530

    Update affiliate_link.py

--- affiliate_link.py
+++ affiliate_link.py
@@ -7,10 +7,11 @@ from config import EDGE_DRIVER_PATH, EDGE_USER_DATA_DIR_PATH, EDGE_PROFILE_DIR
 def get_affiliate_url(product_url):
 
     options = webdriver.EdgeOptions()
-    options.add_argument(f"user-data-dir={EDGE_USER_DATA_DIR_PATH}")
-    options.add_argument(f"profile-directory={EDGE_PROFILE_DIR}")
+    options.add_argument(f"--user-data-dir={EDGE_USER_DATA_DIR_PATH}")
+    options.add_argument(f"--profile-directory={EDGE_PROFILE_DIR}")
     options.add_argument("--disable-gpu")
     options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--headless")  # Optional: Run in headless mode
 
     service = Service(EDGE_DRIVER_PATH)
     driver = webdriver.Edge(service=service, options=options)   
